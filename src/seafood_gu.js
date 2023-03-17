c79c1f4166a96edb4b828845435eea561f316431
Peiyakao <cubearcu@gmail.com>
2023-03-17 16:48:18 +0800

    fixed animate

--- src/seafood_gu.js
+++ src/seafood_gu.js
@@ -1,69 +1,72 @@
-const principleContent=document.querySelector(".main-page article li");
-const principle1btn=document.querySelector(".priciple-1");
-const principle2btn=document.querySelector(".priciple-2");
-const principle3btn=document.querySelector(".priciple-3");
-const principle4btn=document.querySelector(".priciple-4");
-const principle5btn=document.querySelector(".priciple-5");
-const principle6btn=document.querySelector(".priciple-6");
-const principle7btn=document.querySelector(".priciple-7");
-const principle8btn=document.querySelector(".priciple-8");
-const principleContent1=document.querySelector(".priciple-1-article");
-const principleContent2=document.querySelector(".priciple-2-article");
-const principleContent5=document.querySelector(".priciple-5-article");
-const principleContent6=document.querySelector(".priciple-6-article");
-const principleContent7=document.querySelector(".priciple-7-article");
-const principleContent8=document.querySelector(".priciple-8-article");
 
-principle1btn.addEventListener("click",()=>{ 
-    principleContent.style.display="block"
+const p1btn=document.querySelector(".priciple-1");
+const p2btn=document.querySelector(".priciple-2");
+const p3btn=document.querySelector(".priciple-3");
+const p4btn=document.querySelector(".priciple-4");
+const p5btn=document.querySelector(".priciple-5");
+const p6btn=document.querySelector(".priciple-6");
+const p7btn=document.querySelector(".priciple-7");
+const p8btn=document.querySelector(".priciple-8");
+const p1C=document.querySelector(".priciple-1-article");
+const p2C=document.querySelector(".priciple-2-article");
+const p5C=document.querySelector(".priciple-5-article");
+const p6C=document.querySelector(".priciple-6-article");
+const p7C=document.querySelector(".priciple-7-article");
+const p8C=document.querySelector(".priciple-8-article");
+
+p1btn.addEventListener("click",()=>{ 
+    p1C.style.display="block";
+});
+
+p2btn.addEventListener("click",()=>{ 
+    p2C.style.display="block";
+    p1C.style.display="none";
+    
+});
+
+p3btn.addEventListener("click",()=>{ 
+    p2C.style.display="block";
+    p1C.style.display="none";
+   
+    
+});
+p4btn.addEventListener("click",()=>{ 
+    p2C.style.display="block";
+    p1C.style.display="none";
+    
+});
+p5btn.addEventListener("click",()=>{ 
+    p1C.style.display="none";
+    p2C.style.display="none";
+    p5C.style.display="block";
    
 });
 
-principle2btn.addEventListener("click",()=>{ 
-    principleContent.style.display="block"
-    principleContent1.style.display="none"
+p6btn.addEventListener("click",()=>{ 
+    p1C.style.display="none";
+    p2C.style.display="none";
+    p5C.style.display="none";
+    p6C.style.display="block";
+    
     
 });
 
-principle3btn.addEventListener("click",()=>{ 
-    principleContent.style.display="block"
-    principleContent1.style.display="none"
+p7btn.addEventListener("click",()=>{ 
+    p1C.style.display="none";
+    p2C.style.display="none";
+    p5C.style.display="none";
+    p6C.style.display="none";
+    p7C.style.display="block";
+    
     
 });
-principle4btn.addEventListener("click",()=>{ 
-    principleContent.style.display="block"
-    principleContent1.style.display="none"
-    
-});
-principle5btn.addEventListener("click",()=>{ 
-    principleContent.style.display="block"
-    principleContent1.style.display="none"
-    principleContent2.style.display="none"
-   
-});
-
-principle6btn.addEventListener("click",()=>{ 
-    principleContent.style.display="block"
-    principleContent1.style.display="none"
-    principleContent2.style.display="none"
-    principleContent5.style.display="none"
-    
-});
-
-principle7btn.addEventListener("click",()=>{ 
-    principleContent.style.display="block"
-    principleContent1.style.display="none"
-    principleContent2.style.display="none"
-    principleContent5.style.display="none"
-    principleContent6.style.display="none"
-    
-});
-principle8btn.addEventListener("click",()=>{ 
-    principleContent.style.display="block"
-    principleContent1.style.display="none"
-    principleContent2.style.display="none"
-    principleContent5.style.display="none"
-    principleContent6.style.display="none"
-    principleContent7.style.display="none"
+p8btn.addEventListener("click",()=>{ 
+    p1C.style.display="none";
+    p2C.style.display="none";
+    p5C.style.display="none";
+    p6C.style.display="none";
+    p7C.style.display="none";
+    p8C.style.display="block";
+  
     
 });
